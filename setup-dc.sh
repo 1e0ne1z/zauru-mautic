ada2124e55b4e672c9f88f56bc2e58cd0b6513ec
Leonel Velasquez <leonel@zauru.com>
2025-03-11 17:26:02 -0600

    fix docker script

--- setup-dc.sh
+++ setup-dc.sh
@@ -1,6 +1,6 @@
 cd /var/www
-docker compose build
-docker compose up -d db --wait && docker compose up -d mautic_web --wait
+docker-compose build
+docker-compose up -d db --wait && docker-compose up -d mautic_web --wait
 
 echo "## Wait for basic-mautic_web-1 container to be fully running"
 while ! docker exec basic-mautic_web-1 sh -c 'echo "Container is running"'; do
@@ -9,7 +9,7 @@ while ! docker exec basic-mautic_web-1 sh -c 'echo "Container is running"'; do
 done
 
 echo "## Check if Mautic is installed"
-if docker compose exec -T mautic_web test -f /var/www/html/config/local.php && docker compose exec -T mautic_web grep -q "site_url" /var/www/html/config/local.php; then
+if docker-compose exec -T mautic_web test -f /var/www/html/config/local.php && docker-compose exec -T mautic_web grep -q "site_url" /var/www/html/config/local.php; then
     echo "## Mautic is installed already."
 else
     # Check if the container exists and is running
@@ -25,11 +25,11 @@ else
         echo "Container basic-mautic_worker-1 does not exist or is not running."
     fi
     echo "## Installing Mautic..."
-    docker compose exec -T -u www-data -w /var/www/html mautic_web php ./bin/console mautic:install --force --admin_email {{EMAIL_ADDRESS}} --admin_password {{MAUTIC_PASSWORD}} http://{{IP_ADDRESS}}:{{PORT}}
+    docker-compose exec -T -u www-data -w /var/www/html mautic_web php ./bin/console mautic:install --force --admin_email {{EMAIL_ADDRESS}} --admin_password {{MAUTIC_PASSWORD}} http://{{IP_ADDRESS}}:{{PORT}}
 fi
 
 echo "## Starting all the containers"
-docker compose up -d
+docker-compose up -d
 
 DOMAIN="{{DOMAIN_NAME}}"
 
@@ -93,12 +93,12 @@ else
 fi
 
 echo "## Check if Mautic is installed"
-if docker compose exec -T mautic_web test -f /var/www/html/config/local.php && docker compose exec -T mautic_web grep -q "site_url" /var/www/html/config/local.php; then
+if docker-compose exec -T mautic_web test -f /var/www/html/config/local.php && docker-compose exec -T mautic_web grep -q "site_url" /var/www/html/config/local.php; then
     echo "## Mautic is installed already."
     
     # Replace the site_url value with the domain
     echo "## Updating site_url in Mautic configuration..."
-    docker compose exec -T mautic_web sed -i "s|'site_url' => '.*',|'site_url' => 'https://$DOMAIN',|g" /var/www/html/config/local.php
+    docker-compose exec -T mautic_web sed -i "s|'site_url' => '.*',|'site_url' => 'https://$DOMAIN',|g" /var/www/html/config/local.php
 fi
 
 echo "## Script execution completed"
--- setup-vps.sh
+++ setup-vps.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-# Create a swap file
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-sudo sysctl vm.swappiness=10
-echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
-
-# Enable ports for HTTP traffic
-sudo ufw allow 80
-sudo ufw allow 443
-
 # Install Nginx
 sudo apt-get update
-sudo apt-get install -y nginx certbot python3-certbot-nginx vim nano ca-certificates curl gnupg lsb-release
-# Add Docker's official GPG key
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get install -y nginx certbot python3-certbot-nginx vim nano
 
-# Set up Docker repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y docker.io docker-compose
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
-sudo apt-get install -y docker-compose-plugin
-# git --version
-# docker --version
-# docker compose version
+# Install Git
+sudo apt-get install -y git
