45fc19ddbfb845033fcc70d010055ff254eb903a
Leonel Velasquez <leonel@zauru.com>
2025-03-12 14:24:48 -0600

    set missing scripts

--- setup-vps.sh
+++ setup-vps.sh
@@ -11,5 +11,14 @@ sudo apt-get install -y docker.io docker-compose
 # Install Git
 sudo apt-get install -y git
 
-sudo groupadd docker
+# Add ubuntu user to docker group
 sudo usermod -aG docker ubuntu
+
+# Fix docker.sock permissions
+sudo chmod 666 /var/run/docker.sock
+
+# Restart Docker service
+sudo systemctl restart docker
+
+# Force group changes to take effect without logout
+exec sudo su -l ubuntu
